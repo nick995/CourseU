76d450176b880275f157e7770f67d73b6a67c818
Mingyu Jung <mingyu995@gmail.com>
2023-10-19 15:07:54 -0600

    phase 1

---
+++ grades/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from . import models
+# Create your views here.
+
+def assignments(request):
+    return render(request, "assignments.html")
+
+def index(request, assignment_id):
+    return render(request, "index.html")
+
+def login_form(request):
+    return render(request, "login.html")
+
+def profile(request):
+    return render(request, "profile.html")
+
+def submissions(request, assignment_id):
+    return render(request, "submissions.html")
