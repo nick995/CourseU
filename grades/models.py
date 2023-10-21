d6d4e3b0f6a2b57ab2efe2e2ea3864bd45aa650a
Mingyu Jung <mingyu995@gmail.com>
2023-10-21 14:00:26 -0600

    phase 4

--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User, Group
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class Assignment(models.Model):
+    # short string for title
+    title = models.CharField(max_length=200)
+    # long string for the assignment description
+    description = models.TextField(blank=True)
+    # date and a time.
+    deadline = models.DateTimeField()
+    # integer weight (which is how much the assignment is worth toward the final grade)
+    weight = models.IntegerField()
+    # integer number of maximum points(which is how much this assignment is graded out of)
+    points = models.IntegerField(
+        default=100,
+    )
+class Submission(models.Model):
+
+    assignment = models.ForeignKey(Assignment,
+                                   on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    grader = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='graded_set')
+    file = models.FileField()
+    score = models.FloatField(default=0.0, blank=True, null=True)
+
+    
--- grades/views.py
+++ grades/views.py
@@ -3,10 +3,38 @@ from . import models
 # Create your views here.
 
 def assignments(request):
-    return render(request, "assignments.html")
+    assignments = models.Assignment.objects.all()
+    return render(request, 
+                  "assignments.html",
+                  dict(assignments=assignments))
 
 def index(request, assignment_id):
-    return render(request, "index.html")
+
+    assignments = models.Assignment.objects.filter(id=assignment_id).count()
+    total_assignment = models.Assignment.objects.count()
+    # How many total submissions there are to this assignment(assignment_id)
+    submission_total = models.Submission.objects.filter(assignment=assignment_id).count()
+    submission_object = models.Submission.objects.filter(assignment=assignment_id)
+
+    # How many of submissions are assigned to "you" with a name of ta1
+    assign_submission = models.Submission.objects.filter(id=assignment_id)
+    print("assign", assign_submission)
+    total_submissions = models.Submission.objects.filter(id=assignment_id, grader='ta1').count()
+
+    # How many total students there are
+    total_student = models.Group.objects.get(name="Students").user_set.count()
+
+    print("assignment id = " ,assignment_id)
+    print("total = ", total_submissions)
+    return render(request, 
+                 "index.html",
+                 {'assignments': assignments,
+                  'total_assignment': total_assignment,
+                  'total_student': total_student,
+                  'submissions': submission_total,
+                  'sub_object': submission_object,
+                  'assigned_submission': assign_submission
+                  })
 
 def login_form(request):
     return render(request, "login.html")
