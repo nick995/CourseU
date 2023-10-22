8db44d912febd2330fec69c8b2fd421c831db78c
Mingyu Jung <mingyu995@gmail.com>
2023-10-21 22:43:40 -0600

    phase 4 done

--- grades/views.py
+++ grades/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from . import models
+from django.db.models import Count
+
 # Create your views here.
 
 def assignments(request):
@@ -10,30 +12,30 @@ def assignments(request):
 
 def index(request, assignment_id):
 
-    assignments = models.Assignment.objects.filter(id=assignment_id).count()
     total_assignment = models.Assignment.objects.count()
+    
     # How many total submissions there are to this assignment(assignment_id)
     submission_total = models.Submission.objects.filter(assignment=assignment_id).count()
-    submission_object = models.Submission.objects.filter(assignment=assignment_id)
-
     # How many of submissions are assigned to "you" with a name of ta1
-    assign_submission = models.Submission.objects.filter(id=assignment_id)
-    print("assign", assign_submission)
-    total_submissions = models.Submission.objects.filter(id=assignment_id, grader='ta1').count()
-
+    assigned = models.Submission.objects.filter(assignment=assignment_id).filter(grader__username = 'ta1').count()
     # How many total students there are
     total_student = models.Group.objects.get(name="Students").user_set.count()
-
-    print("assignment id = " ,assignment_id)
-    print("total = ", total_submissions)
+    
+    assignment_title = models.Assignment.objects.get(id = assignment_id).title
+    total_points = models.Assignment.objects.get(id = assignment_id).points
+    due_month = models.Assignment.objects.get(id = assignment_id).deadline.strftime('%B')
+    due_day = models.Assignment.objects.get(id = assignment_id).deadline.strftime('%d')
+    
     return render(request, 
                  "index.html",
-                 {'assignments': assignments,
-                  'total_assignment': total_assignment,
+                 {
                   'total_student': total_student,
                   'submissions': submission_total,
-                  'sub_object': submission_object,
-                  'assigned_submission': assign_submission
+                  'assigned': assigned,
+                  'assignment_title': assignment_title,
+                  'total_points': total_points,
+                  'due_day': due_day,
+                  'due_month': due_month
                   })
 
 def login_form(request):
