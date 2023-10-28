1eb8ede37ca29730b708ff54414b8cecdf1a4b1f
Mingyu Jung <mingyu995@gmail.com>
2023-10-27 21:06:18 -0600

    before pull

--- grades/views.py
+++ grades/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from . import models
 from django.db.models import Count
-
+from django.http import Http404, HttpResponse
 # Create your views here.
 
 def assignments(request):
@@ -12,20 +12,21 @@ def assignments(request):
 
 def index(request, assignment_id):
 
-    total_assignment = models.Assignment.objects.count()
-    
-    # How many total submissions there are to this assignment(assignment_id)
-    submission_total = models.Submission.objects.filter(assignment=assignment_id).count()
-    # How many of submissions are assigned to "you" with a name of ta1
-    assigned = models.Submission.objects.filter(assignment=assignment_id).filter(grader__username = 'ta1').count()
-    # How many total students there are
-    total_student = models.Group.objects.get(name="Students").user_set.count()
-    
-    assignment_title = models.Assignment.objects.get(id = assignment_id).title
-    total_points = models.Assignment.objects.get(id = assignment_id).points
-    due_month = models.Assignment.objects.get(id = assignment_id).deadline.strftime('%B')
-    due_day = models.Assignment.objects.get(id = assignment_id).deadline.strftime('%d')
-    
+    try:    
+        # How many total submissions there are to this assignment(assignment_id)
+        submission_total = models.Submission.objects.filter(assignment=assignment_id).count()
+        # How many of submissions are assigned to "you" with a name of ta1
+        assigned = models.Submission.objects.filter(assignment=assignment_id).filter(grader__username = 'ta1').count()
+        # How many total students there are
+        total_student = models.Group.objects.get(name="Students").user_set.count()
+        
+        assignment_title = models.Assignment.objects.get(id = assignment_id).title
+        total_points = models.Assignment.objects.get(id = assignment_id).points
+        due_month = models.Assignment.objects.get(id = assignment_id).deadline.strftime('%B')
+        due_day = models.Assignment.objects.get(id = assignment_id).deadline.strftime('%d')
+        description = models.Assignment.objects.get(id = assignment_id).description
+    except:
+        raise Http404("...")
     return render(request, 
                  "index.html",
                  {
@@ -35,7 +36,8 @@ def index(request, assignment_id):
                   'assignment_title': assignment_title,
                   'total_points': total_points,
                   'due_day': due_day,
-                  'due_month': due_month
+                  'due_month': due_month,
+                  'description': description
                   })
 
 def login_form(request):
