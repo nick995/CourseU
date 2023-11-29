8e3f640fb7b2fd8c32adb672f5fc379e7ebf3908
Mingyu Jung <mingyu995@gmail.com>
2023-11-29 16:32:48 -0700

    phase 1 test

--- cs3550/settings.py
+++ cs3550/settings.py
@@ -116,9 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'grades/templates/'
-STATICFILES_DIRS = [ 'grades/templates/' ]
+# STATIC_URL = 'grades/templates/'
+# STATICFILES_DIRS = [ 'grades/templates/' ]
 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [ 'static/' ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
