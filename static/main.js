9da14a69c6a8b3da7e4718b7602fa1c6f5b79c03
Mingyu Jung <mingyu995@gmail.com>
2023-11-29 22:53:43 -0700

    phase 1

--- static/main.js
+++ static/main.js
@@ -1,3 +1,13 @@
-export function say_hi() {
-    console.log("Hello");
+import { $ } from "/static/jquery/src/jquery.js";
+
+//  This imports the $ function from the jQuery library. 
+//  In jQuery the $ function is used to wrap HTML nodes with jQuery features.
+// export function say_hi() {
+//     console.log("Hello");
+// }
+
+export function say_hi(elt) {
+    console.log("Say hi to", elt);
 }
+
+say_hi($("h1"));
